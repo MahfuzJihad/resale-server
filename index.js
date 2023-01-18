de9876fd0544400e8c16cc6eb27282d8b8e98b5a
jihadmahfuz <jihadmah166@gmail.com>
2023-01-18 13:13:22 +0600

    final commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json())
 
 //mongoDb atlas
-const uri = "mongodb+srv://jihad:<jihad>@cluster0.bgttmgf.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://jihad:jihad@cluster0.bgttmgf.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function verifyJWT(req, res, next) {
@@ -35,99 +35,99 @@ async function verifyJWT(req, res, next) {
 async function run() {
 
     try {
-        const categories = client.db('resaleapp').collection('categories');
-        const products = client.db('resaleapp').collection('products');
-        const booked_products = client.db('resaleapp').collection('booked_products');
-        const users = client.db('resaleapp').collection('users');
-        const advertises = client.db('resaleapp').collection('advertises');
+        const Categories = client.db('resaleapp').collection('categories');
+        const Products = client.db('resaleapp').collection('products');
+        const Booked_products = client.db('resaleapp').collection('booked_products');
+        const Users = client.db('resaleapp').collection('users');
+        const Advertises = client.db('resaleapp').collection('advertises');
 
         app.get('/cetegories', async (req, res) => {
             const query = {};
-            const result = await categories.find(query).toArray();
+            const result = await Categories.find(query).toArray();
             res.send(result);
         })
 
         app.post('/productAdd', async (req, res) => {
             const review = req.body;
-            const result = await products.insertOne(review);
+            const result = await Products.insertOne(review);
             res.send(result)
         })
 
         app.get('/cetegories/:id', async (req, res) => {
             const product = req.params.id;
             const query = { id: product };
-            const result = await products.find(query).toArray();
+            const result = await Products.find(query).toArray();
             res.send(result);
 
         })
 
         app.get('/allProduct', async (req, res) => {
             const query = {};
-            const result = await products.find(query).toArray();
+            const result = await Products.find(query).toArray();
             res.send(result)
         })
         app.get('/allProduct/:email', async (req, res) => {
             const user = req.params.email
             const query = { email: user };
-            const result = await products.find(query).toArray();
+            const result = await Products.find(query).toArray();
             res.send(result)
         })
 
         app.post('/booking', async (req, res) => {
             const boking = req.body;
-            const result = await booked_products.insertOne(boking);
+            const result = await Booked_products.insertOne(boking);
             res.send(result);
         })
         
         app.post('/advertic', async (req, res) => {
             const boking = req.body;
-            const result = await advertises.insertOne(boking);
+            const result = await Advertises.insertOne(boking);
             res.send(result);
         })
         
         app.get('/advertic/:email', async (req, res) => {
             const email = req.params.email;
             const query = { email: email };
-            const result = await advertises.find(query).toArray();
+            const result = await Advertises.find(query).toArray();
             res.send(result);
         })
         
         app.get('/bookings/:email', async (req, res) => {
             const email = req.params.email;
             const query = { email: email };
-            const result = await booked_products.find(query).toArray();
+            const result = await Booked_products.find(query).toArray();
             res.send(result);
         })
         
         app.get('/bookings', async (req, res) => {
             const query = {};
-            const result = await booked_products.find(query).toArray();
+            const result = await Booked_products.find(query).toArray();
             res.send(result);
         })
 
         app.post('/users', async (req, res) => {
-            const users = req.body;
-            const result = await users.insertOne(users);
+            const user = req.body;
+            const result = await Users.insertOne(user);
             res.send(result);
         })
 
         app.get('/users', async (req, res) => {
             const query = {};
-            const result = await users.find(query).toArray();
+            const result = await Users.find(query).toArray();
             res.send(result)
         })
         
         app.get('/users/:email', async (req, res) => {
             const email = req.params.email;
             const query = { email: email };
-            const result = await users.find(query).toArray();
+            const result = await Users.find(query).toArray();
             res.send(result)
         })
         
         app.get('/jwt', async (req, res) => {
             const email = req.query.email;
             const query = { email: email };
-            const user = await users.findOne(query);
+            const user = await Users.findOne(query);
             if (user) {
                 const token = jwt.sign({ email }, process.env.ACCSS_TOKEN, { expiresIn: '1h' });
                 return res.send({ accessToken: token })
@@ -150,4 +150,4 @@ run().catch(error => console.log(error))
 app.get('/', (req, res) => {
     res.send('Hello from resale application')
 })
-app.listen(port, () => console.log(`Resale applicationserver running at: ${port}`))
+app.listen(port, () => console.log(`Resale applicationserver running at: http://localhost${port}`))
